40ce783f8ea5a2e49bb76d2704ff25244a1d6d44
Joshua Bussdieker <jbussdieker@gmail.com>
2013-06-01 14:44:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CambRails::Application.config.secret_token = '59d0da45f852d136971d86105abd5b274d48fc30fa76d838e9d4911b2d302bc8fcf6ea712984feee68922f3178f4b45ef544629d8eca958638195a4fc0c31b74'
